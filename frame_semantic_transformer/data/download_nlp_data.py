bfd5fe4ec037801046395df9b644e18576d86695
David Chanin <chanindav@gmail.com>
2022-05-25 13:44:52 +0100

    don't bother installing wordnet data

--- frame_semantic_transformer/data/download_nlp_data.py
+++ frame_semantic_transformer/data/download_nlp_data.py
@@ -2,7 +2,7 @@ import nltk
 
 
 def ensure_nlp_data_downloaded() -> None:
-    for corpus in ["framenet_v17", "wordnet"]:
+    for corpus in ["framenet_v17"]:
         try:
             nltk.data.find(f"corpora/{corpus}")
         except LookupError:
