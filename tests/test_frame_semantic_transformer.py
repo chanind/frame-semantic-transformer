8cf8e32d2151fa938c194647793588d24fb5a11c
David Chanin <chanindav@gmail.com>
2023-02-27 17:32:00 +0000

    fix: fixing errors when no frames are found

--- tests/test_frame_semantic_transformer.py
+++ tests/test_frame_semantic_transformer.py
@@ -3,13 +3,29 @@ from syrupy.assertion import SnapshotAssertion
 from frame_semantic_transformer import FrameSemanticTransformer
 
 
+transformer = FrameSemanticTransformer("small")
+
+
 def test_basic_detect_frames_functionality(snapshot: SnapshotAssertion) -> None:
-    transformer = FrameSemanticTransformer("small")
     result = transformer.detect_frames(
         "I'm getting quite hungry, but I can wait a bit longer."
     )
     assert result.sentence == "I'm getting quite hungry, but I can wait a bit longer."
     assert result == snapshot
+
+
+def test_problematic_sentence() -> None:
+    result = transformer.detect_frames("two cars parked on the sidewalk on the street")
+    assert result.sentence == "two cars parked on the sidewalk on the street"
+    assert len(result.trigger_locations) > 0
+    assert len(result.frames) > 0
+
+
+def test_no_results() -> None:
+    result = transformer.detect_frames("nope")
+    assert result.sentence == "nope"
+    assert len(result.trigger_locations) == 0
+    assert len(result.frames) == 0
 
 
 def test_basic_detect_frames_bulk() -> None:
@@ -18,7 +34,6 @@ def test_basic_detect_frames_bulk() -> None:
         "The chef gave the food to the customer.",
         "The hallway smelt of boiled cabbage and old rag mats.",
     ]
-    transformer = FrameSemanticTransformer("small")
     results = transformer.detect_frames_bulk(sentences)
     assert len(results) == 3
     for result, sentence in zip(results, sentences):
