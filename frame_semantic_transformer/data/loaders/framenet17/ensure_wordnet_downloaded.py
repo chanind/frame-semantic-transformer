343906c3a0df59cd111355d8aa84d33b39003459
David Chanin <chanindav@gmail.com>
2023-03-15 01:29:40 +0000

    fix: auto-download omw-1.4 for inference

--- frame_semantic_transformer/data/loaders/framenet17/ensure_wordnet_downloaded.py
+++ frame_semantic_transformer/data/loaders/framenet17/ensure_wordnet_downloaded.py
@@ -6,3 +6,7 @@ def ensure_wordnet_downloaded() -> None:
         nltk.data.find("corpora/wordnet.zip")
     except LookupError:
         nltk.download("wordnet")
+    try:
+        nltk.data.find("corpora/omw-1.4.zip")
+    except LookupError:
+        nltk.download("omw-1.4")
